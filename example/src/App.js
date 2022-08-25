7cd6feaa3734b16714e7423a5c0121e5679f64ec
Patrice-H <webmestre64@gmail.com>
2022-08-25 09:52:54 +0200

    Creating component

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from '@patrice-h/custom-modal'
+import Modal from '@patrice-h/custom-modal'
 import '@patrice-h/custom-modal/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Modal />
 }
 
 export default App
